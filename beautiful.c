42d0da388dd37a1708331e06767fc57354fed6f0
Your Name <you@example.com>
2023-05-27 11:58:29 -0700

    change

--- beautiful.c
+++ beautiful.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ *  test - prints "Hello world"
+ *
+ * This function prints the message "Hello world" to the console.
+ * It does not take any arguments or return any value.
+ *return: void
+ */
 
 void test(void)
 {
 	printf("Hello world");
 }
+
+
+
