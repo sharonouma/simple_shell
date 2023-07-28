336b2d615980c8167915e8722f7e5317c0396b62
Your Name <you@example.com>
2023-07-28 05:47:34 -0700

    shell

--- cmd.c
+++ cmd.c
@@ -11,12 +11,6 @@ int (*builtin_func[]) (char **) = {
 	&shell_cd, &shell_help,
 	&shell_exit, &shell_ctrld, &_setenv, &_unsetenv};
 
-/**
- * shell_exit - exits the shell
- * @argv: the command-line arguments
- *
- * Return: 0 to signal to exit the shell
- */
 int shell_exit(char **argv)
 {
 	(void)argv;
@@ -24,12 +18,6 @@ int shell_exit(char **argv)
 	return (0);
 }
 
-/**
- * shell_cd - changes the current directory
- * @args: the command-line arguments
- *
- * Return: 0 on success, -1 on failure
- */
 int shell_cd(char **args)
 {
 	if (args[1])
@@ -47,11 +35,7 @@ int shell_cd(char **args)
 
 	return (0);
 }
-/**
- * shell_ctrld - builtin to handle "^D" call
- * @args: List of args.  Not examined.
- * Return: Always returns 0, to terminate execution.
- */
+
 int shell_ctrld(char **args)
 {
 	(void)args;
@@ -59,21 +43,11 @@ int shell_ctrld(char **args)
 	return (200);
 }
 
-/**
- * num_builtins - size
- * Return: size
- */
-
 int num_builtins(void)
 {
 	return (sizeof(builtins) / sizeof(char *));
 }
 
-/**
- * shell_help - prints the help for the shell
- * @args: List of args.  Not examined.
- * Return: Always returns 1, to continue executing.
- */
 int shell_help(char **args)
 {
 	int i;
@@ -87,17 +61,6 @@ int shell_help(char **args)
 
 	return (1);
 }
-
-/**
- *_fork - foo that create a fork.
- *@argc: Command and values path.
- *@argv: Has the name of our program.
- *@env: Environment
- *@line: Command line for the user.
- *@pd: ID of proces.
- *@check: Checker add new test
- *Return: 0 success
- */
 
 int _fork(char **argc, char **argv, char **env, char *line, int pd, int check)
 {
