36bff0e91b853f7c32ebdd03f8128dbfc660b38d
Your Name <you@example.com>
2023-07-28 04:16:10 -0700

    commit

---
+++ beautiful.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * test - prints hello world
+ * Return: void
+ */
+void print_hello_world(void)
+{
+    printf("Hello world");
+}
+
+int main(void)
+{
+    print_hello_world();
+
+    return (0);
+}
+
